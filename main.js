aa455a32261a3e18e2ce4f6c2daa448d75b9eca5
thanhtuyen3007 <112745568+thanhtuyen3007@users.noreply.github.com>
2024-03-16 23:27:08 +0700

    menu tags

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ function showModal() {
 function hideModal() {
   myModal.style.display = "none";
 }
+// Toggle between hiding and showing blog replies/comments
+function showReplies(x) {
+  const blogReplies = document.querySelectorAll(".blog__replies");
+  [...blogReplies].map((blogReplieItem, index) => {
+    blogReplieItem.classList.toggle("active");
+  });
+}
+function liked(x) {
+  x.classList.toggle("active");
+}
